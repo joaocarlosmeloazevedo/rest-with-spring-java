a6f119ff862f74da5d41d5bcd24604fe3ad5225b
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2024-04-18 23:26:31 -0300

    Project Created using Spring Tool Suite

---
+++ 01_FirstStepsJavaSpringBoot/rest-with-spring-java/src/test/java/br/com/neutron/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.neutron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
