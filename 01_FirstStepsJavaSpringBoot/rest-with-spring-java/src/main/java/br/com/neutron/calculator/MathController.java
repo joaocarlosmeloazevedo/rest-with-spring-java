4a5acf8ebf8bab87089e9d7f3f4301d7626b665e
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2024-04-22 21:56:19 -0300

    Sum function implemented

--- 01_FirstStepsJavaSpringBoot/rest-with-spring-java/src/main/java/br/com/neutron/calculator/MathController.java
+++ 01_FirstStepsJavaSpringBoot/rest-with-spring-java/src/main/java/br/com/neutron/calculator/MathController.java
@@ -15,8 +15,28 @@ public class MathController {
 	
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
 	public Double sum (@PathVariable(value = "numberOne") String numberOne,
-					   @PathVariable(value = "numberTwo") String numberTwo) { //TO-DO String por enquanto para tratar exceções
-			
-		return 1D;
+					   @PathVariable(value = "numberTwo") String numberTwo)
+	throws Exception{
+	
+	if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		throw new Exception();
+	}
+	return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	
+	}
+	
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D; //Double zerado
+		
+		String number = strNumber.replaceAll(",", ".");
+		
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
+	}
+	
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 }
