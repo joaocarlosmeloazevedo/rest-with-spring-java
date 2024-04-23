087a7a8daf730c3a788e4350fc43eb3b2a44b70a
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2024-04-22 22:16:33 -0300

    Generic and Bad Request Exceptions implemented

--- 01_FirstStepsJavaSpringBoot/rest-with-spring-java/src/main/java/br/com/neutron/calculator/MathController.java
+++ 01_FirstStepsJavaSpringBoot/rest-with-spring-java/src/main/java/br/com/neutron/calculator/MathController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.neutron.exceptions.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
 //	private static final String template = "Hello, %s!";
@@ -19,7 +21,7 @@ public class MathController {
 	throws Exception{
 	
 	if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-		throw new Exception();
+		throw new UnsupportedMathOperationException("Please set a numeric value!");
 	}
 	return convertToDouble(numberOne) + convertToDouble(numberTwo);
 	
