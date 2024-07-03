8f6bacc57363c68589438ec617a72337a57ae0ba
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2024-07-03 11:22:10 -0300

    Service Class implemented with Logger

---
+++ 01_FirstStepsJavaSpringBoot/rest-with-spring-java/src/main/java/br/com/neutron/services/PersonServices.java
@@ -0,0 +1,27 @@
+package br.com.neutron.services;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.neutron.model.Person;
+
+@Service //Instanciação da classe via Annotation SERVICE.
+public class PersonServices {
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public Person findById (String id) {
+		
+		logger.info("Finding one person");
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Joao");
+		person.setLastName("Azevedo");
+		person.setAddress("SP São Paulo");
+		person.setGender("Masculino");
+		return new Person();
+	}
+}
