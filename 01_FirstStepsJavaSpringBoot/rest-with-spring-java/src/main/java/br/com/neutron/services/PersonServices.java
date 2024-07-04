799f0c7ffba89ac78fcabedd854de174273d8f2c
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2024-07-04 11:45:45 -0300

    List of Person and return list method created with mocked data

--- 01_FirstStepsJavaSpringBoot/rest-with-spring-java/src/main/java/br/com/neutron/services/PersonServices.java
+++ 01_FirstStepsJavaSpringBoot/rest-with-spring-java/src/main/java/br/com/neutron/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.neutron.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,19 @@ public class PersonServices {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	public List<Person> findAll (String id) {
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+		
+	}
+	private Person mockPerson(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	public Person findById (String id) {
 		
 		logger.info("Finding one person");
